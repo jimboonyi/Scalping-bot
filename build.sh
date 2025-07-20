b9b5f4a6407db936933d85a1c021d0cbb4ac75ba
jimboonyi <jimboonyi30@gmail.com>
2025-07-20 13:17:15 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
-set -o errexit
+set -e
 
-# Install TA-Lib dependencies
-apt-get update
-apt-get install -y build-essential wget
+# Create writable directories for apt
+mkdir -p $HOME/.apt/{lists,archives}
+export APT_DIR=$HOME/.apt
 
-# Download and install TA-Lib
-wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
-tar -xzf ta-lib-0.4.0-src.tar.gz
+# Configure apt to use custom directories
+echo "Dir::State \"$APT_DIR\";
+Dir::State::status \"$APT_DIR/status\";
+Dir::Cache \"$APT_DIR\";
+Dir::Cache::archives \"$APT_DIR/archives\";" > $APT_DIR/apt.conf
+
+# Install system dependencies
+apt-get -o Dir::Etc::SourceList=/dev/null update
+apt-get -o Dir::Etc::SourceList=/dev/null install -y \
+    build-essential \
+    wget \
+    python3-dev
+
+# Install TA-Lib in user space
+TA_LIB_DIR=$HOME/ta-lib
+mkdir -p $TA_LIB_DIR
+wget -q http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar xzf ta-lib-0.4.0-src.tar.gz
 cd ta-lib/
-./configure --prefix=/usr
-make
+./configure --prefix=$TA_LIB_DIR
+make -j$(nproc)
 make install
 cd ..
-rm -rf ta-lib-0.4.0-src.tar.gz ta-lib
+rm -rf ta-lib*
 
 # Install Python dependencies
-pip install -r requirements.txt
+pip install --user -r requirements.txt
+
+# Set environment variables
+echo "export TA_LIBRARY_PATH=$TA_LIB_DIR/lib" >> $HOME/.bashrc
+echo "export LD_LIBRARY_PATH=$TA_LIB_DIR/lib:\$LD_LIBRARY_PATH" >> $HOME/.bashrc
